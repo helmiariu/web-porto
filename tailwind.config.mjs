806e486c553bc2f959b81656153f39fbc928591f
Helmi Arimbawa <helmiarimbawa46@gmail.com>
2026-05-19 22:14:40 +0700

    sidebar-v8

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"], // Pastikan path ini mencakup semua folder
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
