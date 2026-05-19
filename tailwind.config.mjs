a55df82690071e078fc13530727c566fa9733925
Helmi Arimbawa <helmiarimbawa46@gmail.com>
2026-05-19 16:50:40 +0700

    commit pertama

---
+++ tailwind.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"], // Pastikan path ini mencakup semua folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
