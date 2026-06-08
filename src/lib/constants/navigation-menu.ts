21d3a814ae58318fe72ded451377202647d17fd5
Helmi Arimbawa <helmiarimbawa46@gmail.com>
2026-06-08 23:35:42 +0700

    feat: add contact and myAI pages and update navigation menu configuration

--- src/lib/constants/navigation-menu.ts
+++ src/lib/constants/navigation-menu.ts
@@ -7,6 +7,9 @@ import {
   Rotate3d,
   Gauge,
   Menu,
+  MessageSquareText,
+  Rss,
+  Bot
 } from "@lucide/astro";
 
 export type SidebarMenu = {
@@ -25,15 +28,18 @@ export const publicDashboardMenu: SidebarMenu[] = [
       { href: "/", label: "Home", icon: Home },
       { href: "/project", label: "Project", icon: Briefcase },
       { href: "/3Dgallery", label: "3D Gallery", icon: Rotate3d },
-      { href: "/monitoring", label: "Monitoring", icon: Gauge },
-      { href: "/blog", label: "Blog", icon: FileText },
+
+      { href: "/blog", label: "Blog", icon: Rss },
+      { href: "/contact", label: "Contact", icon: MessageSquareText },
     ],
   },
   {
     groupLabel: "Application",
     menus: [
       // { href: "/ai", label: "AI" },
+      { href: "/myAI", label: "My AI", icon: Bot },
       { href: "/chat", label: "Chat", icon: MessageCircle },
+      { href: "/monitoring", label: "Monitoring", icon: Gauge },
     ],
   },
   // {
